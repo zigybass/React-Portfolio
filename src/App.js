88b22e179e59892ab0f73bbb58be3a0d18a3ea19
Zigy <zigybass@gmail.com>
2019-10-09 17:36:43 -0400

    added header comp with button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Home from "../src/components/Home";
+import Header from "../src/components/Header"
 
 function App() {
+
   return (
+    <>
+    <Header></Header>
     <Home></Home>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header () {
+
+    return (
+        <>
+        <div>
+            <button>Home</button>
+        </div>
+        </>
+    )
+
+}
+
+export default Header;
