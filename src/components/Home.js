1d164dc584f6c1cb4439e73ec9db8d388c6ff3a9
Zigy <zigybass@gmail.com>
2019-10-11 18:54:51 -0400

    added favicon.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
         <div className="row">
           <div className="col">
             <div className="jumbotron home">
-              <h1>Software Developer</h1>
+              <h1>Chris Zeigler</h1>
               <br></br>
               <h4>Full-Stack Developer</h4>
               <h4>React • Node.js • Express • MySQL/Mongo</h4>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ function Portfolio () {
             <div className="container">
                 <div className="jumbotron home">
                     <h1>Portfolio</h1>
-                    <p>Projects go here</p>
+                    
                 </div>
             </div>
         </div>
