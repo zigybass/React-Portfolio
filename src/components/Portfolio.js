929f5ced7901ddb6fbf1a2c662794fea1dbf95d7
Zigy <zigybass@gmail.com>
2019-10-12 14:53:38 -0400

    added style to images. added grid system to projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PetLife from "../images/petlife.png"
 
 function Portfolio () {
     return (
@@ -6,7 +7,26 @@ function Portfolio () {
             <div className="container">
                 <div className="jumbotron home">
                     <h1>Portfolio</h1>
-                    
+                    <div className="row">
+                    <div className="col-lg-4">
+                    <img className="pics" src={PetLife} alt="petlife"/>
+                    <div>
+                        <h4>PetLife Project</h4>
+                    </div>
+                    </div>
+                    <div className="col-lg-4">
+                    <img className="pics" src={PetLife} alt="petlife"/>
+                    <div>
+                        <h4>PetLife Project</h4>
+                    </div>
+                    </div>
+                    <div className="col-lg-4">
+                    <img className="pics" src={PetLife} alt="petlife"/>
+                    <div>
+                        <h4>PetLife Project</h4>
+                    </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
