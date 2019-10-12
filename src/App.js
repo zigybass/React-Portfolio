af851c600f6a9874bdbac67ce8fdcbe43fbe529c
Zigy <zigybass@gmail.com>
2019-10-12 14:54:48 -0400

    added rest of the files. terminal restarted

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React from "react";
 import Home from "../src/components/Home";
 import "../src/css/style.css";
-import { NavLink, Router } from "react-router-dom";
-// import Contact from "../src/components/Contact"
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Contact from "./components/Contact";
+import None from "./components/None";
+import Portfolio from "./components/Portfolio";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+// import Pdf from "../src/resume.pdf";
+
 
 class App extends React.Component {
+  state = {};
 
-  state = {}
-  
-  render () {
-  return (
-    <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
+  componentDidMount () {
+    console.log("mounted!")
+  }
+
+  render() {
+    return (
+      <>
         <Router>
-        <NavLink to="/">Home></NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route component={None} />
+          </Switch>
         </Router>
-      </div>
-    </nav>
-    <Home />
-    </>
-  );
-}
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
