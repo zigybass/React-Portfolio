d0e385f4ef8e80f8734ddeac25a4593405304859
Zigy <zigybass@gmail.com>
2019-10-16 12:10:11 -0400

    Updates

---
+++ precache-manifest.35f05c826187fa87f132cd066d0d3f96.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf64796251541b08d569a9f7b3cc802",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "f88e67dbf37662f98166",
+    "url": "/React-Portfolio/static/css/main.9a018394.chunk.css"
+  },
+  {
+    "revision": "3ea0c75629929f79b42a",
+    "url": "/React-Portfolio/static/js/2.e0dfcfbb.chunk.js"
+  },
+  {
+    "revision": "f88e67dbf37662f98166",
+    "url": "/React-Portfolio/static/js/main.73828316.chunk.js"
+  },
+  {
+    "revision": "4e7228d1241dbe46ea7e",
+    "url": "/React-Portfolio/static/js/runtime-main.fa175e6a.js"
+  },
+  {
+    "revision": "b9adae214a5a7bb607e72045ddc4704e",
+    "url": "/React-Portfolio/static/media/fanclubNEW.b9adae21.png"
+  },
+  {
+    "revision": "9b8c138995b605f8fa47f5b22aff781a",
+    "url": "/React-Portfolio/static/media/friendfinder.9b8c1389.png"
+  },
+  {
+    "revision": "3ee39a98391ab0c3829cfe89fb27b4d7",
+    "url": "/React-Portfolio/static/media/mountain.3ee39a98.jpg"
+  },
+  {
+    "revision": "ac3e8afda43e94d47ee13ddd5e1df097",
+    "url": "/React-Portfolio/static/media/petlifeNEW.ac3e8afd.png"
+  },
+  {
+    "revision": "ac2ed242dc3412ab3703af8f4df62bdc",
+    "url": "/React-Portfolio/static/media/resume.ac2ed242.pdf"
+  },
+  {
+    "revision": "bb9de6d46207fdd8e9d2e25a7fa9d6b7",
+    "url": "/React-Portfolio/static/media/shoppingapp.bb9de6d4.png"
+  },
+  {
+    "revision": "a3661f4096fe0ec41852316d33b19941",
+    "url": "/React-Portfolio/static/media/travelguideNEW.a3661f40.png"
+  }
+]);
