5cb8e5dd09ce2af2440190a59c206cfec04c2099
Zigy <zigybass@gmail.com>
2019-10-16 13:11:01 -0400

    added containers and overlay to rest of portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,45 +15,75 @@ function Portfolio() {
           <h1>Portfolio</h1>
           <div className="row">
             <div className="col-lg-4">
-              <div className="container portImage">
+              <div className="container">
+              <div className="portImage">
               <img className="pics" src={PetLife} alt="petlife" />
               <div className="overlay" >
                 <div className="text"></div>
               </div>
               </div>
-              <div>
+              <div className="container">
                 <h4>PetLife</h4>
                 <p className="appInfo" >React/Node/MongoDB/Authentication </p>
               </div>
-            </div>
-            <div className="col-lg-4">
-              <img className="pics" src={FanClub} href="" alt="petlife" />
-              <div>
-                <h4>Fan Club</h4>
-                <p className="appInfo">Express/MySQL/Sequelize</p>
               </div>
             </div>
             <div className="col-lg-4">
+              <div className="container">
+              <div className="portImage">
+              <img className="pics" src={FanClub} href="" alt="petlife" />
+              <div className="overlay" >
+                <div className="text"></div>
+              </div>
+              </div>
+              <div className="container">
+                <h4>Fan Club</h4>
+                <p className="appInfo">Express/MySQL/Sequelize</p>
+              </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="container">
+              <div className="portImage">
               <img className="pics" src={TravelGuide} alt="travelGuide" />
-              <div>
+              <div className="overlay" >
+                <div className="text"></div>
+              </div>
+              </div>
+              <div className="container">
                 <h4>Travel Guide</h4>
                 <p className="appInfo">Node/APIs/Bootstrap</p>
+              </div>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-lg-4">
+              <div className="container">
+              <div className="portImage">
               <img className="pics" src={FriendFinder} alt="friendFinder" />
+              <div className="overlay" >
+                <div className="text"></div>
+              </div>
+              </div>
               <div>
                 <h4>Friend Finder</h4>
                 <p className="appInfo">Matching algorithm in JS</p>
               </div>
+              </div>
             </div>
             <div className="col-lg-4">
+              <div className="container">
+              <div className="portImage">
               <img className="pics" src={ShoppingApp} href="" alt="shoppingApp" />
+              <div className="overlay">
+                <div className="text"></div>
+              </div>
+              </div>
               <div>
                 <h4>Shopping App</h4>
                 <p className="appInfo">Node/Express/MySQL/Sequelize</p>
+              </div>
               </div>
             </div>
             <div className="col-lg-4">
