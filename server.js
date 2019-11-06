d09e6de26edbf184bd1929d47f6a7d72cbbd0052
Zigy <zigybass@gmail.com>
2019-11-06 15:35:53 -0500

    added hi res pic and changed css

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/test", (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.json({
+    message: "Testing route"
+  })
+})
 
 app.listen(PORT, function () {
     console.log("App listening on Port: " + PORT);
