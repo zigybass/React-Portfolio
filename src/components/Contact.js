6148fe011529a2eccd8c701922b4d7c672787024
Zigy <zigybass@gmail.com>
2019-11-21 17:07:38 -0500

    added FontAwesome npm. Started adding FA icons to Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/style.css";
+import FontAwesome from "react-fontawesome";
 // import Pdf from "../resume.pdf";
 
 function Contact () {
@@ -11,9 +12,11 @@ function Contact () {
                     <div className="jumbotron home">
                         <h1>Contact info</h1>
                         <div className="contactMain">
-                            <p>Email: <span>zigybass@gmail.com</span></p>
-                            <p>GitHub: <a href="https://github.com/zigybass" target="blank">Here</a></p>
-                            <p>LinkedIn: <a href="https://www.linkedin.com/in/christopher-zeigler/" target="blank">Here</a></p>
+                            <p><FontAwesome className="fas fa-envelope-square tip" size="2x"><span className="tiptext">Email</span> </FontAwesome><span>zigybass@gmail</span></p>
+
+                            <p><a href="https://github.com/zigybass" target="blank"><FontAwesome className="fab fa-github tip" size="2x"><span className="tiptext">GitHub</span></FontAwesome></a></p>
+
+                            <p><a href="https://www.linkedin.com/in/christopher-zeigler/" target="blank"><FontAwesome className="fab fa-linkedin" size="2x"/></a></p>
                             <p>Resume: <a href="https://docs.google.com/document/d/1-ldSKJHM86tnsuqRcYcVZkZHLaW4hn0Iqzuz6An0KxA/edit?ts=5d8a3022" target="blank">Here</a> </p>
                         </div>
                     </div>
